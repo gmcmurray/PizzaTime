02a7c05c4114c693d2798da7e5b023cad7edaaca
gmcmurray <83099840+gmcmurray@users.noreply.github.com>
2021-11-10 05:13:24 -0800

    modified Kitchen page to include form to add phone orders, still need to connect

--- client/src/pages/KitchenQ.js
+++ client/src/pages/KitchenQ.js
@@ -5,9 +5,11 @@ import { useQuery } from '@apollo/client';
 import { QUERY_KITCHENQUEUE } from '../utils/queries';
 import mockdata from '../utils/mockdata.json';
 import Clock from '../components/Clock';
-
+import {ADD_ORDER } from '../utils/mutations';
+import { ADD_ORDER_KITCHEN } from '../utils/mutations';
 import  '../utils/table.css'
 import { useStoreContext } from '../utils/GlobalState';
+import Dropdown from '../components/DropDown';
 
 function Table ({columns, data}) {
   const {
@@ -22,7 +24,6 @@ function Table ({columns, data}) {
       })
 
     return (
-      
         <table {...getTableProps()}>
         <thead>
           {headerGroups.map(headerGroup => (
@@ -51,7 +52,62 @@ function Table ({columns, data}) {
 }
 
   function KitchenQ() {
+      const [addFormData, setAddFormData] = useState({
+        orderName: "",
+        pizzas:""
+      })
+      const [show, setShow] = React.useState(true)
       
+
+      const handleAddFormChange = (event)=>{
+        event.preventDefault();
+
+
+        const fieldName = event.target.getAttribute('name');
+        const fieldValue = event.target.value;
+
+        const newFormData = { ...addFormData};
+        newFormData[fieldName] = fieldValue;
+
+        setAddFormData(newFormData);
+
+      }
+
+      async function schedulePizza(event){
+        event.preventDefault();
+        setShow(false)
+        // Reveal Add/Cancel Buttons
+        // Go and schedule pizza 
+        // Return commit time and either add order or cancel
+        console.log("Schedule that pizza!")
+      }
+
+      async function orderPizza(event){
+        event.preventDefault();
+       
+        setTimeout(function() {
+          setShow(true)
+        }, 1500);
+        
+        // Order the pizza
+        // Add to the kitchen
+        // const  {data} = await addOrder(
+        // { variables: { products: productIds } });
+        console.log("Order that pizza!")
+      }
+
+      function cancelPizza(event){
+        event.preventDefault();
+        setTimeout(function() {
+          setShow(true)
+        }, 1500);
+        
+        // Cancel Order
+        // Clear form , wait 3 seconds and show Schedule button 
+       
+        console.log("Cancel that pizza!")
+      }
+
       let {loading, data:quedata} = useQuery(QUERY_KITCHENQUEUE, 
         {variables: {today: new Date().toLocaleDateString().slice(0,10)}});
       const  bqueue = quedata?.kitchentoday.queue|| [];
@@ -59,7 +115,6 @@ function Table ({columns, data}) {
       if(bqueue){
       console.log("quedata",quedata) 
       console.log(bqueue)
-      
       for (let x = 0;x < bqueue.length; x++) {
                 ddata[x]={
                   priority: bqueue[x].priority,
@@ -73,16 +128,11 @@ function Table ({columns, data}) {
         else{
          ddata = mockdata;
         }
-
     //Define columns
     const columns = React.useMemo(() => [
         {
             Header: 'Pizza #',
             accessor: (row, i) =>i+1,
-        },
-        {
-            Header: 'Order #',
-            accessor: 'orderId'
         },
         {
           Header: 'Order Name',
@@ -92,24 +142,32 @@ function Table ({columns, data}) {
             Header: 'Item Ordered',
             accessor: 'pizzas'
         },
-        
         {
             Header: 'Priority',
             accessor: 'priority'
         },
-        
         {
             Header: 'Commit Time',
             accessor: 'commitTime'
+        },
+        {
+          Header: 'Status '
+        },
+        {
+          Header: 'Action '
         }
-
     ])
-     
+    const items =[{id:"61738d66bad24764ccfd820f",
+                  value:"MeatLovers"},
+                  {id:"61738d66bad24764ccfd820e",
+                    value:"Vegi"},
+                    {id:"61738d66bad24764ccfd8210",
+                      value:"Combo"}]
+                      
     const data = React.useMemo(() => ddata)
 
     return(
         <>
-
         <div className="container my-1">
         <Link to="/"> ← Back to Pizza Menus</Link>
         <br />
@@ -117,12 +175,33 @@ function Table ({columns, data}) {
             <h1>
                 Kitchen Queue    <br /><br />
             </h1>
-
+            
             <h3>
                 Orders currently in the kitchen today {new Date().toLocaleDateString().slice(0,10)} <Clock /><br /><br />
             </h3>
-          
-            <Table columns={columns} data={data}/>
+            <h4> Phone Order: </h4> 
+            <div style={{margin: '4px',justifyContent: 'space-evenly', display: 'flex', border: ' 2px solid red'}}>
+            <form > 
+              <input type="text" name="orderName" required="required" 
+              placeholder="OrderName ..." onChange={handleAddFormChange} />
+              <input type="text" name="phoneNumber" required="required" 
+              placeholder="PhoneNumber ..." onChange={handleAddFormChange} />
+              <input type="text" name="TimeWanted" required="required" 
+              placeholder="Time Wanted ..." onChange={handleAddFormChange} />
+              <input type="text" name="TimeCommit" required="required" 
+              placeholder="Time Commit ..." onChange={handleAddFormChange} />
+            </form>
+            <Dropdown title="Select Pizzas" items={items}/>
+             { show ?
+              <button className="btnSch" type="submit" onClick={schedulePizza} > <span>Sched</span> </button> 
+              : <div>
+              <button className="btnOrder" type="submit" onClick={orderPizza}> <span className="btnText">Add</span> </button>
+              <button className="btnCancel" type="submit" onClick={cancelPizza}> <span className="btnText">Cancel</span> </button>
+             
+             </div>}
+              </div>
+            
+            <Table columns={columns} data={data} multiSelect/>
             
         </div>
         </>
